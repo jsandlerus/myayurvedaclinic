b8ef36a33f27ecfb690100040ff55fb1cb15de40
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2021-08-26 17:56:04 -0700

    Updates

---
+++ precache-manifest.824584c617aa3410063ff0b3c9747098.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1297795d2bd65aeabfd6e81f160a68b",
+    "url": "/myayurvedaclinic/index.html"
+  },
+  {
+    "revision": "f286bb6de9320883a3a7",
+    "url": "/myayurvedaclinic/static/css/main.57b2870c.chunk.css"
+  },
+  {
+    "revision": "11ffd80241f9565515a1",
+    "url": "/myayurvedaclinic/static/js/2.9d27a93d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/myayurvedaclinic/static/js/2.9d27a93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f286bb6de9320883a3a7",
+    "url": "/myayurvedaclinic/static/js/main.ca57f826.chunk.js"
+  },
+  {
+    "revision": "8208a35563334c014d3e",
+    "url": "/myayurvedaclinic/static/js/runtime-main.9f84d215.js"
+  },
+  {
+    "revision": "2b4e147c4f001a47c47a9f1874421a8a",
+    "url": "/myayurvedaclinic/static/media/AldoOva.2b4e147c.png"
+  },
+  {
+    "revision": "c6265c9b5ecfdec647c9b4c533098fc6",
+    "url": "/myayurvedaclinic/static/media/AlejandraLopez.c6265c9b.png"
+  },
+  {
+    "revision": "8df17fc4c41e743f8783ea68b1775da9",
+    "url": "/myayurvedaclinic/static/media/Ayurveda_Health_Logo.8df17fc4.png"
+  },
+  {
+    "revision": "a70b8fe6baf078440978d87299cb493b",
+    "url": "/myayurvedaclinic/static/media/CarmenClavelArcas.a70b8fe6.png"
+  },
+  {
+    "revision": "332544397f560b750af9ce36d3cd35e4",
+    "url": "/myayurvedaclinic/static/media/CindyLeonCasab2.33254439.png"
+  },
+  {
+    "revision": "a8caafa301556db926a99e3b3332c760",
+    "url": "/myayurvedaclinic/static/media/FranciscoOttoni.a8caafa3.png"
+  },
+  {
+    "revision": "4360923e2f9b001ef7699ecb607114c0",
+    "url": "/myayurvedaclinic/static/media/HectorBarbosa.4360923e.png"
+  },
+  {
+    "revision": "f48484877305a060f30f8024bb566792",
+    "url": "/myayurvedaclinic/static/media/IndyMena.f4848487.png"
+  },
+  {
+    "revision": "f82c9f6a2c961fdd79da2b137af5c329",
+    "url": "/myayurvedaclinic/static/media/LauraZampedri.f82c9f6a.png"
+  },
+  {
+    "revision": "deff483827d6a621456c233f2ae89b45",
+    "url": "/myayurvedaclinic/static/media/MariaJosePreisler.deff4838.png"
+  },
+  {
+    "revision": "23027f5256997c6413fd470619f38836",
+    "url": "/myayurvedaclinic/static/media/Mendozaflor.23027f52.png"
+  },
+  {
+    "revision": "479ea3ad2ca394535e0e5234632b6aad",
+    "url": "/myayurvedaclinic/static/media/VirginiaAlvarez.479ea3ad.png"
+  },
+  {
+    "revision": "d80cb10e626ec6dee47cf394bd282fe1",
+    "url": "/myayurvedaclinic/static/media/WendySanchez.d80cb10e.png"
+  }
+]);
